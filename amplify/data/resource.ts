be0eb83953f685efb40c433572668048d18c7410
Venkatavaradhan Viswanathan <10469649+venkatavaradhanv@users.noreply.github.com>
2024-02-02 17:47:34 -0500

    Update resource.ts

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -11,8 +11,10 @@ const schema = a.schema({
   Todo: a
     .model({
       content: a.string(),
+      done: a.boolean(),
+      priority: a.enum(['low', 'medium', 'high'])
     })
-    .authorization([a.allow.owner(), a.allow.public().to(['read'])]),
+    .authorization([a.allow.owner()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -20,11 +22,7 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'apiKey',
-    // API Key is used for a.allow.public() rules
-    apiKeyAuthorizationMode: {
-      expiresInDays: 30,
-    },
+    defaultAuthorizationMode: 'userPool'
   },
 });
 
